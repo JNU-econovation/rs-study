4598798064740d7847975b06b2010bca3f5d0d97
Hyeonji Jang <jhg050908@naver.com>
2023-02-19 01:36:25 +0900

    update : 2주차 과제에 주석 추가

--- this-is-coding-test/Hyeonz1/2주차/greedy/greedy2.py
+++ this-is-coding-test/Hyeonz1/2주차/greedy/greedy2.py
@@ -2,11 +2,13 @@ n, m = map(int, input().split())
 
 cards = [list(map(int, input().split())) for _ in range(n)]
 
+# cards[i]의 min값을 비교해서 가장 높은 값이 있는 행i값을 선택한다.
+    # cards[i]를 오름차순으로 정렬한다.
+    # cards[i][0]를 비교해서 가장 큰 값을 찾는다.
+
 for i in range(n):
     cards[i].sort()
-cards.sort()
 
-print(cards)
 mymax = 0
 
 for i in range(n - 1):
--- this-is-coding-test/Hyeonz1/2주차/greedy/greedy3.py
+++ this-is-coding-test/Hyeonz1/2주차/greedy/greedy3.py
@@ -1,12 +1,18 @@
 n, k = map(int, input().split())
 
-index = 0
+# n이 1이 될 때까지 작업의 최소횟수
+    # 작업
+        # 1. n - 1
+        # 2. n / k (단, 나누어 떨어질 때만 사용 가능)
+    # 1이 아닌 어떤 수가 k로 주어지더라도 n을 k로 나누는 것이 n에서 1을 빼는 것보다 숫자를 더 많이 줄인다.
+
+count = 0
 while n != 1:
     if (n % k == 0):
         n //= k
-        index += 1
+        count += 1
     else:
         n -= 1
-        index += 1
+        count += 1
 
-print(index)
+print(count)
