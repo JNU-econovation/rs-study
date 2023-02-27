69c2d878a1ad44fe137647ce2a077261515ed3a1
Daesun Hwang <hds650588@gmail.com>
2023-02-27 13:52:44 +0900

    feat : 유기농 배추 문제

---
+++ baekjoon/유기농 배추.py
@@ -0,0 +1,30 @@
+t = int(input())
+
+
+def dfs(graph,r,c):
+    if(-1<r<n and -1<c<m):
+        if(graph[r][c] == 1):
+            graph[r][c] = 0
+            dfs(graph,r-1,c)
+            dfs(graph,r+1,c)
+            dfs(graph,r,c+1)
+            dfs(graph,r,c-1)
+            return 1
+        return 0
+
+
+for tt in range(t): #t번 반복
+
+    m,n,k = map(int,input().split())
+    count = 0
+
+    graph = [[0 for i in range(m)] for _ in range(n)]
+
+    for kk in range(k):
+        c, r = map(int,input().split())
+        graph[r][c] = 1
+    
+    for nn in range(n):
+        for mm in range(m):
+            count += dfs(graph,nn,mm)
+    print(count)
