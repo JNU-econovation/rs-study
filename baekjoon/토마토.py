bd3d7dcd8d40af2140541e7978a4f2b1dc65fa89
hwangdaesun <hds650588@gmail.com>
2023-03-03 22:21:53 +0900

    fix : 토마토 문제

--- baekjoon/토마토.py
+++ baekjoon/토마토.py
@@ -9,58 +9,40 @@ dir = ((1,0,0),(-1,0,0),(0,1,0),(0,-1,0),(0,0,1),(0,0,-1))
 queue = deque()
 
 def bfs(i,j,k):
-    for d in range(len(dir)):
-        ii = i + dir[d][0]
-        jj = j + dir[d][1]
-        kk = k + dir[d][2]
-        if(-1<ii<h and -1<jj<n and -1<kk<m):
-            if(box[ii][jj][kk] == 0):
-                queue.append((ii,jj,kk))  
+    queue.append((i,j,k))
+    while(queue):
+        ii,jj,kk = queue.popleft()
+        for d in range(len(dir)):
+            ai = ii + dir[d][0]
+            aj = jj + dir[d][1]
+            ak = kk + dir[d][2]
+            if(-1<ai<h and -1<aj<n and -1<ak<m):
+                if(box[ai][aj][ak] == 0):
+                    queue.append((ai,aj,ak))
+                    box[ai][aj][ak] = box[ii][jj][kk] + 1 
+    
                 
 
-
-b = 0
 for i in range(h):
     for j in range(n):
         for k in range(m):
-            if(box[i][j][k] == 0):
-                b +=1
-if(b==0):
-    print(0)
-    sys.exit()
-
+            if(box[i][j][k] == 1):
+                bfs(i,j,k)
 count = 0
-stop = False
-
-while(1):
-    for i in range(h):
-        for j in range(n):
-            for k in range(m):
-                if(box[i][j][k] == 1):
-                    bfs(i,j,k)
-                    if queue:
-                        a = queue.popleft()
-                        box[a[0]][a[1]][a[2]] = 1
-                        count += 1
-                    else:
-                        stop = True
-    if(stop):
-        break
-
-
-print(box)
 
 for i in range(h):
     for j in range(n):
+        num = max(box[i][j])
+        if(num > count):
+            count = num
         for k in range(m):
             if(box[i][j][k] == 0):
                 print(-1)
                 sys.exit()
-                
-        
-print(count)
 
-# bfs에서의 함수는 방문을 했는 지의 여부를 확인한다
+
+print(count-1)
+
 
 
 
